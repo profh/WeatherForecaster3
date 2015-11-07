5b61e23c108da738494ebe7f713e6d43c089c6ec
profh <profh@cmu.edu>
2015-11-06 21:31:59 -0500

    Initial Commit

---
+++ WeatherForecaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherForecaster
+//
+//  Created by Larry Heimann on 11/6/15.
+//  Copyright (c) 2015 Larry Heimann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
